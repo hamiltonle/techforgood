25ee4d8acbbe71b6aa43f6446b827ed4d56fc5cc
Amin Malik <ammalik17@gmail.com>
2017-06-26 18:30:07 +0800

    Set up routes for users, courses, organizations, enrollments, sesisons and rooted to courses index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users do
+    collection do
+      get 'dashboard', to: "users#dashboard"
+    end
+  end
+
+  resources :courses do
+    resources :lessons, shallow: true
+  end
+
+  resources :organizations
+
+  resources :enrollments, only: [:index, :new, :create, :destroy]
+
+  resources :sessions, only: [:show, :create, :update]
+
+  root to: "courses#index"
+
 end
