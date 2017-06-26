7bea6d1c7e6da15bb442ce5497c8a58db62745e0
Hamilton Le <hamilton.m.le@gmail.com>
2017-06-26 17:02:53 +0800

    added annotations

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,8 +1,11 @@
 class EnrollmentsController < ApplicationController
+  
+  # finds all enrolled students in a course
   def index
     @enrollments = Enrollment.all
   end
 
+  # creates a new enrollment once a student signs up to a course
   def create
     @course = Course.find(params[:course_id])
     @enrollment = @course.enrollments.new
@@ -10,6 +13,7 @@ class EnrollmentsController < ApplicationController
     @enrollment.save
   end
 
+  # deletes an enrollment if the student decides to cancel the class
   def destroy
     @enrollment = Enrollment.find(params[:id])
     @enrollment.delete
