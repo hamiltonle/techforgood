bdf893c30875bceabf24d2702531af8d129841b8
Hamilton Le <hamilton.m.le@gmail.com>
2017-07-04 17:26:22 +0800

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -354,8 +354,9 @@ puts "saved a course"
 puts "Creating lessons"
 
 lesson = Lesson.new(
-  title: "The Disability  injustice",
+  title: "A growing concern in China",
   text: "If you judged the country by its laws alone, China would be a global leader on disability rights. The ‘Laws on the Protection of Persons with Disabilities’, introduced in 1990, offer strong and wide-ranging protection of the civil rights of the disabled, guaranteeing employment, education, welfare, and access. But despite the high concerns of the law, Chinese cities make little concession to disabled people. As the sociologist Yu Jianrong has documented, raised pathways for the blind often lead into dead ends, bollards, trees or open pits, or else spiral decoratively but misleadingly. Wheelchair access is non-existent, especially outside Beijing or Shanghai, and guide dogs are effectively forbidden from most public spaces, despite the authorities’ repeated promises of full access.",
+  description: "What's the situation like for Chinese children with physical disabilities",
   lesson_type: "lecture",
   video: "<embed src='http://player.youku.com/player.php/sid/XMTU5OTM0MTIxMg==/v.swf' allowFullScreen='true' quality='high' width='100%' height='400' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>",
   minutes: 15,
@@ -368,6 +369,7 @@ puts "saved a lesson"
 lesson = Lesson.new(
   title: "Zhanyijihua's Solution",
   text: "Through the design, the production of economical simple 3D printing machinery prosthetics, and open source and dissemination of this technology, the wing plan hopes to help millions of children with physical disability in China.",
+  description: "An introduction to our social impact",
   lesson_type: "quiz",
   video: "",
   minutes: 10,
@@ -401,6 +403,7 @@ The e-NABLE Community is an amazing group of individuals from all over the world
 They are people who have put aside their political, religious, cultural and personal differences – to come together and collaborate on ways to help improve the open source 3D printable designs for hands and arms for those who were born missing fingers or who have lost them due to war, disease or natural disaster.
 
 The e-NABLE Community is made up of  teachers, students, engineers, scientists, medical professionals, tinkerers, designers, parents, children, scout troops, artists, philanthropists, dreamers, coders, makers and every day people who just want to make a difference and help to “Give The World A Helping Hand.”",
+  description: "How a small group of people are helping thousands",
   lesson_type: "lecture",
   video: "<embed src='http://player.youku.com/player.php/sid/XNTYzODA4Mzk2/v.swf' allowFullScreen='true' quality='high' width='100%' height='400' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>",
   minutes: 15,
@@ -415,7 +418,6 @@ lesson = Lesson.new(
   text: "
 Learn a little about the grassroots, open-source e-NABLE Community and our history!
 
-aboutusfinal
 
 
 
@@ -448,6 +450,7 @@ In 2011, my husband Ivan Owen, created a crazy metal functional puppet hand to w
 A simple Youtube video of the hand, led to an email from a carpenter named Richard from South Africa,who had lost his fingers in a woodworking accident and a collaboration across 10,000 miles to create a replacement finger for him that lasted nearly a year. They worked through various protolesson_types and designs via skype and email, using objects they could both find around their homes and respective countries.
 
 This ultimately led to a mother of a 5 year old boy contacting them to see about the creation of a tiny version of this hand for a little boy named Liam who lives in South Africa who was born with no fingers on his right hand.",
+  description: "An introduction to our partners stories",
   lesson_type: "article",
   video: "",
   minutes: 10,
@@ -465,6 +468,7 @@ lesson = Lesson.new(
   text: "This course will demonstrate how 3D printers work, show what people make with them, and examine the 3D printing ecosystem. It will also explore the future of 3D printing and discuss how this technology will revolutionize our world. The course materials include informative video lectures, on-location interviews with a variety of 3D printing experts, and engaging hands-on exercises. Learners who complete this introductory course will have a solid understanding of 3D printing and its revolutionary potential, and will be able to print and customize 3D designs. In addition, learners will obtain a preview of the other courses in the 3D Printing Specialization and gain an appreciation for how these courses will help them develop the knowledge and skills to turn their ideas into objects.
 
 This course was listed in the top 50 MOOC's of all time by Class-Central (https://www.class-central.com/report/top-moocs/)",
+  description: "How 3D printing is a game changer",
   lesson_type: "lecture",
   video: "<embed src='http://player.youku.com/player.php/sid/XNjMxMTExMTQ0/v.swf' allowFullScreen='true' quality='high' width='100%' height='400' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>",
   minutes: 20,
@@ -478,6 +482,7 @@ puts "saved a lesson"
 lesson = Lesson.new(
   title: "3D Printing Applications",
   text: "This course will help you understand how 3D printing is being applied across a number of domains, including design, manufacturing, and retailing. It will also demonstrate the special capabilities of 3D printing such as customization, self-assembly, and the ability to print complex objects. In addition to business applications, this course will also examine how individuals, including those in developing countries, are using this technology to create solutions to the problems they face. This course will also provide an overview of design thinking and how you can use this framework to develop ideas that can be turned into objects. Learners who complete this course will obtain a rich understanding of the capabilities of 3D printing and how to think about designing objects for this new technology.",
+  description: "How are leading players using 3D printing technology",
   lesson_type: "article",
   video: "",
   minutes: 10,
@@ -491,7 +496,7 @@ lesson = Lesson.new(
   title: "How 3D printers work",
   text: "Nullam quis risus eget urna mollis ornare vel eu leo. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec ullamcorper nulla non metus auctor fringilla.
     Donec id elit non mi porta gravida at eget metus. Nullam quis risus eget urna mollis ornare vel eu leo. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.",
-  description: "Cras mattis consectetur purus sit amet fermentum.",
+  description: "What are the mechinics of a 3D printing machine",
   lesson_type: "quiz",
   video: "",
   minutes: 30,
@@ -505,6 +510,7 @@ lesson = Lesson.new(
   title: "3D Printing Software",
   text: "This course will demonstrate how to use 3D printing software to create digital designs that can be turned into physical objects. It will also demonstrate how 3D scanners work to turn physical objects into digital designs. This course is hands-on in nature and will provide step-by-step instructions to guide you through two popular 3D modeling programs, Tinkercad and Fusion 360. Learners who complete this course will be able to use 3D software to design a wide variety of objects for both personal and professional use. In addition, learners who enroll in the course certificate will receive extended free access to Fusion 360 (provided by Autodesk).
 ",
+  description: "How to write your own scripts for your 3D printer",
   lesson_type: "lecture",
   video: "<embed src='http://player.youku.com/player.php/sid/XNTAyOTcwMzgw/v.swf' allowFullScreen='true' quality='high' width='100%' height='400' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>",
   minutes: 10,
@@ -519,6 +525,7 @@ lesson = Lesson.new(
   text: "This course will provide an in-depth exploration of desktop 3D printing hardware. It will examine the history of desktop 3D printing and demonstrate how 3D printers are made and how they work. This course will also provide step-by-step instructions for how to use and repair a 3D printer. It will also explore the different lesson_types of materials that can be 3D printed and will demonstrate how you can create various objects using these tools and materials. Learners who complete this course will be able to successfully operate, repair, and upgrade a 3D printer. In addition, learners who enroll in the course certificate will be able to purchase a desktop 3D printer at a discounted price (provided by Ultimaker).
 
 If you enjoy this business course and are interested in an MBA, consider applying to the iMBA, a flexible, fully-accredited online MBA at an incredibly competitive price offered by the University of Illinois. For more information, please see the Resource page in this course and onlinemba.illinois.edu.",
+  description: "What makes up a 3D printer?",
   lesson_type: "assignment",
   video: "",
   minutes: 30,
@@ -531,6 +538,7 @@ puts "saved a lesson"
 lesson = Lesson.new(
   title: "3D Printing Capstone",
   text: "The capstone will integrate the learning acquired from the four courses in the 3D Printing Specialization through a hands-on project. This project will enable you to turn an idea into an object using the knowledge and skills acquired through the other courses. Specifically, you will be guided through a step-by-step process in which you imagine, design, make, and share a 3D printed object. Thus, through this capstone, you will be able to put course concepts into action and utilize the skills that you have acquired throughout the Specialization. In addition, you will be able to obtain an actual 3D print of the object you design at a discounted price through our corporate partners, 3D Hubs and Shapeways.",
+  description: "Your final project for the course",
   lesson_type: "discussion",
   video: "",
   minutes: 15,
@@ -547,7 +555,7 @@ lesson = Lesson.new(
   title: "How Computer Vision Works",
   text: "Nullam quis risus eget urna mollis ornare vel eu leo. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec ullamcorper nulla non metus auctor fringilla.
     Donec id elit non mi porta gravida at eget metus. Nullam quis risus eget urna mollis ornare vel eu leo. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.",
-  description: "Cras mattis consectetur purus sit amet fermentum.",
+  description: "An introduction to Microsoft's technology",
   lesson_type: "lecture",
   video: "<embed src='http://player.youku.com/player.php/sid/XMTU5OTM0MTIxMg==/v.swf' allowFullScreen='true' quality='high' width='100%' height='400' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>",
   minutes: 30,
@@ -561,7 +569,7 @@ lesson = Lesson.new(
   title: "How Computer Vision Works",
   text: "Nullam quis risus eget urna mollis ornare vel eu leo. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec ullamcorper nulla non metus auctor fringilla.
     Donec id elit non mi porta gravida at eget metus. Nullam quis risus eget urna mollis ornare vel eu leo. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.",
-  description: "Cras mattis consectetur purus sit amet fermentum.",
+  description: "How face recognition works",
   lesson_type: "lecture",
   video: "<embed src='http://player.youku.com/player.php/sid/XMTU5OTM0MTIxMg==/v.swf' allowFullScreen='true' quality='high' width='100%' height='400' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>",
   minutes: 30,
@@ -575,7 +583,7 @@ lesson = Lesson.new(
   title: "How AR work",
   text: "Nullam quis risus eget urna mollis ornare vel eu leo. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec ullamcorper nulla non metus auctor fringilla.
     Donec id elit non mi porta gravida at eget metus. Nullam quis risus eget urna mollis ornare vel eu leo. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.",
-  description: "Cras mattis consectetur purus sit amet fermentum.",
+  description: "What is Augmented Reality?",
   lesson_type: "lecture",
   video: "<embed src='http://player.youku.com/player.php/sid/XMTU5OTM0MTIxMg==/v.swf' allowFullScreen='true' quality='high' width='100%' height='400' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>",
   minutes: 30,
@@ -589,7 +597,7 @@ lesson = Lesson.new(
   title: "How AR work",
   text: "Nullam quis risus eget urna mollis ornare vel eu leo. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec ullamcorper nulla non metus auctor fringilla.
     Donec id elit non mi porta gravida at eget metus. Nullam quis risus eget urna mollis ornare vel eu leo. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.",
-  description: "Cras mattis consectetur purus sit amet fermentum.",
+  description: "Is AR the Future?",
   lesson_type: "lecture",
   video: "<embed src='http://player.youku.com/player.php/sid/XMTU5OTM0MTIxMg==/v.swf' allowFullScreen='true' quality='high' width='100%' height='400' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>",
   minutes: 30,
