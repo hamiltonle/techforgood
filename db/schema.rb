70c93f12d51e4c3bd78c2ebc38c84bcd55a2329c
Kirsten Judd <kirstensophie@gmail.com>
2017-07-06 09:28:28 +0800

    Got popup working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704015124) do
+ActiveRecord::Schema.define(version: 20170705034342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20170704015124) do
     t.integer "minutes"
     t.string "module_name"
     t.string "lesson_type"
+    t.string "question"
     t.index ["course_id"], name: "index_lessons_on_course_id"
   end
 
@@ -112,6 +113,7 @@ ActiveRecord::Schema.define(version: 20170704015124) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "enrollment_id"
+    t.string "answer"
     t.index ["enrollment_id"], name: "index_sessions_on_enrollment_id"
     t.index ["lesson_id"], name: "index_sessions_on_lesson_id"
     t.index ["user_id", "lesson_id"], name: "index_sessions_on_user_id_and_lesson_id", unique: true
