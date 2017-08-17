e66adfdb28ec631be87abfdee3b96787bab2d533
Amin Malik <ammalik17@gmail.com>
2017-08-17 18:38:57 +0800

    Added start_date attribute to courses and updated sidebar to show course start date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170812195735) do
+ActiveRecord::Schema.define(version: 20170817102744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20170812195735) do
     t.string "tagline"
     t.string "issue"
     t.string "tech_solution"
+    t.date "start_date"
     t.index ["organization_id"], name: "index_courses_on_organization_id"
   end
 
