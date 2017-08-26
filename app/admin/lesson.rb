a55553092d2f6c55bb366a5edde14c6da6fdfb82
Hamilton Le <hamilton.m.le@gmail.com>
2017-08-25 19:36:20 -0500

    basics of showing cloudinary phtos

--- app/admin/lesson.rb
+++ app/admin/lesson.rb
@@ -1,13 +1,17 @@
 ActiveAdmin.register Lesson do
-  permit_params [:title, :video, :text, :description, :course_id]
+  permit_params [:title, :video, :text, :description, :course_id, :score]
 
   form do |f|
     f.inputs "Lesson" do
+      f.input :course_id, :as => :select, :collection => Course.all
+      f.input :module_name, :as => :select, :collection => ["why","what","how"]
+      f.input :lesson_type, :as => :select, :collection => ["assignment","article","lecture","discussion","quiz"]
       f.input :title
+      f.input :score
       f.input :video
-      f.input :text, as: :html_editor
       f.input :description
-      f.input :course_id
+      f.input :text, as: :html_editor
+
     end
     f.actions
   end
